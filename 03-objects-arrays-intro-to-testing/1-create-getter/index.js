36b91d4cb69cb4c8edaec43f77411e00cda1561b
Kontur <sequence@myrealm.ca>
2021-10-06 11:12:29 -0600

    Implemented export function createGetter(path) {

--- 03-objects-arrays-intro-to-testing/1-create-getter/index.js
+++ 03-objects-arrays-intro-to-testing/1-create-getter/index.js
@@ -4,5 +4,17 @@
  * @returns {function} - function-getter which allow get value from object by set path
  */
 export function createGetter(path) {
-
+  let split = path.split(".");
+  let traverseDepth = split.length;
+  return function(obj) {
+    let objQuery = obj;
+    function traverse(obj, index) {
+      if (index == traverseDepth || obj === undefined) {
+        return obj;
+      }
+      obj = obj[split[index]];
+      return traverse(obj, ++index);
+    }
+    return traverse(objQuery, 0);
+  }
 }
